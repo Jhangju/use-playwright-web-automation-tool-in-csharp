046dcabfc1ca00beb31b97f8d325851003914711
unknown <whitecollorhack@gmail.com>
2021-04-26 14:02:05 +0500

    first

---
+++ playwright/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PlaywrightSharp;
+
+namespace playwright
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+                 var playwright = await Playwright.CreateAsync();
+                 var browser = await playwright.Chromium.LaunchAsync(headless: false);
+
+                
+                var page = await browser.NewPageAsync();
+                await page.GoToAsync("https://google.com/");
+                Console.ReadLine();
+        }
+    }
+}
